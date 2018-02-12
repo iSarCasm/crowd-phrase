b34bf4b79b1c195a1bfe029c3975f3566f0f7bb3
SarCasm <sarcasm008@gmail.com>
2018-02-12 16:03:57 +0200

    Add phrases max count (eql 10)

--- app/controllers/concerns/phrases_cookie_storage.rb
+++ app/controllers/concerns/phrases_cookie_storage.rb
@@ -4,12 +4,19 @@ module PhrasesCookieStorage
   included do
     private
 
+    MAX_PHRASES_COUNT = 10
+
     def phrases
       JSON.parse(cookies[:phrases]) rescue []
     end
 
     def set_phrases(val)
-      cookies[:phrases] = val.to_json
+      cookies[:phrases] = val.to_json if val&.size <= MAX_PHRASES_COUNT
     end
+
+    def phrases_left
+      MAX_PHRASES_COUNT - phrases.size
+    end
+    helper_method :phrases_left
   end
 end
